df4c91194a84712319ea9df2d8046cbd690258e1
ahmetenesbahar <enesbahar2303@gmail.com>
2022-09-06 20:06:00 +0300

    while scrolling navbar coloring need fix and  about section need fix

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,3 +7,27 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+window.onscroll = () => {
+  colorFunction();
+};
+
+const portfolio = document.getElementById("portfolio");
+const about = document.getElementById("about");
+const contact = document.getElementById("contact");
+
+const colorPortfolio = portfolio.offsetTop;
+const colorAbout = about.offsetTop;
+const colorContact = contact.offsetTop;
+
+const colorFunction = () => {
+  if (window.pageYOffset > colorPortfolio && window.pageYOffset < colorAbout) {
+    navPortfolio.classList.add("coloring");
+  }
+  if (window.pageYOffset > colorAbout && window.pageYOffset < colorContact) {
+    navbar.classList.add("coloring");
+  }
+  if (window.pageYOffset > colorContact) {
+    navbar.classList.add("coloring");
+  }
+};
