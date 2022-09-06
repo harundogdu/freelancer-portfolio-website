f29b6a3cc2de8b9f283be17f9ae0c7966eee209d
Harun Doğdu <Harundogdu06@gmail.com>
2022-09-06 21:38:42 +0300

    feat(freelancer): custom scroll event created

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,33 +1,38 @@
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
-  });
-});
-
 window.onscroll = () => {
   colorFunction();
 };
 
-const portfolio = document.getElementById("portfolio");
-const about = document.getElementById("about");
-const contact = document.getElementById("contact");
+const portfolio = document.getElementById('portfolio');
+const about = document.getElementById('about');
+const contact = document.getElementById('contact');
+const list = document.querySelector('#navbar ul');
 
-const colorPortfolio = portfolio.offsetTop;
-const colorAbout = about.offsetTop;
-const colorContact = contact.offsetTop;
+const portfolioOffsetTop = portfolio.offsetTop - 10;
+const aboutOffsetTop = about.offsetTop;
+const contactOffsetTop = contact.offsetTop;
 
 const colorFunction = () => {
-  if (window.pageYOffset > colorPortfolio && window.pageYOffset < colorAbout) {
-    navPortfolio.classList.add("coloring");
-  }
-  if (window.pageYOffset > colorAbout && window.pageYOffset < colorContact) {
-    navbar.classList.add("coloring");
-  }
-  if (window.pageYOffset > colorContact) {
-    navbar.classList.add("coloring");
+  if (
+    window.pageYOffset >= portfolioOffsetTop &&
+    window.pageYOffset < aboutOffsetTop
+  ) {
+    list.children[0].classList.add('active');
+    list.children[1].classList.remove('active');
+    list.children[2].classList.remove('active');
+  } else if (
+    window.pageYOffset >= aboutOffsetTop &&
+    window.pageYOffset < contactOffsetTop
+  ) {
+    list.children[1].classList.add('active');
+    list.children[0].classList.remove('active');
+    list.children[2].classList.remove('active');
+  } else if (window.pageYOffset >= contactOffsetTop) {
+    list.children[2].classList.add('active');
+    list.children[0].classList.remove('active');
+    list.children[1].classList.remove('active');
+  } else {
+    list.children[0].classList.remove('active');
+    list.children[1].classList.remove('active');
+    list.children[2].classList.remove('active');
   }
 };
